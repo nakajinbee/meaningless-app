26786db4e0b8adb03a451707b2f0972741d682a3
nakaji <nakaji.m.0625@gmail.com>
2022-10-16 15:54:01 +0900

    フォームののボタン作成

--- meaningless-app/resources/js/pages/mudaEdit.jsx
+++ meaningless-app/resources/js/pages/mudaEdit.jsx
@@ -5,14 +5,14 @@ export default function MudaEdit(){
         <>
             {/* ページタイトル部分 共通部品*/}
             <div className={"page-title-area"}>
-                <div className={"page-title-text"}>
-                    <div className={"page-"}>無駄登録画面</div>
-                    <div>無駄を登録することができます。</div>
+                <div className={"page-title-text-area"}>
+                    <div className={"page-title-text"}>無駄登録画面</div>
+                    <div className={"page-subtitle-text"}>無駄を登録することができます。</div>
                 </div>
                     {/* ボタンエリア */}
                     <div className={"page-top-btn-area"}>
-                        <div>登録ボタン</div>
-                        <div>戻るボタン</div>
+                        <button className={"primary-button form-main-button"}>登録</button>
+                        <button className={"secondary-button form-main-button"}>戻る</button>
                     </div>
                     {/* ボタンエリア */}
             </div>            
