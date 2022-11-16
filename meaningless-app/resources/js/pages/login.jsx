4cda3feada8e15a7bd071099fbd9492718bf9f19
nakaji <nakaji.m.0625@gmail.com>
2022-11-16 23:32:43 +0900

    ログインフォーム作成

--- meaningless-app/resources/js/pages/login.jsx
+++ meaningless-app/resources/js/pages/login.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 import CardHeader from "react-bootstrap/esm/CardHeader";
 
 export default function Login() {
@@ -14,7 +14,19 @@ export default function Login() {
                     <Card.Header>ログイン</Card.Header>
                     <Card.Body>
                         <Card.Title></Card.Title>
-                        <Card.Text></Card.Text>
+                        <Card.Text>
+                            <form>
+                                <div>
+                                    <h5>ログインID</h5>
+                                    <input />
+                                </div>
+                                <div style={{ marginTop: "10px" }}>
+                                    <h5>パスワード</h5>
+                                    <input />
+                                </div>
+                                <button className="">LOGIN</button>
+                            </form>
+                        </Card.Text>
                     </Card.Body>
                 </Card>
             </div>
