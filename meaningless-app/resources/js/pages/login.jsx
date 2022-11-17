a2b3b3b46270001b7505d22a59226d43b6adb7ff
nakaji <nakaji.m.0625@gmail.com>
2022-11-17 23:28:04 +0900

    ログインフォーム一部修正

--- meaningless-app/resources/js/pages/login.jsx
+++ meaningless-app/resources/js/pages/login.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import CardHeader from "react-bootstrap/esm/CardHeader";
@@ -7,22 +8,37 @@ export default function Login() {
         'meta[name="csrf-token"]'
     ).content;
 
+    const [loginId, setLoginId] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <>
             <div className="login-page">
                 <Card border="dark" className="login-card">
-                    <Card.Header>ログイン</Card.Header>
+                    <Card.Header>ログインフォーム</Card.Header>
                     <Card.Body>
                         <Card.Title></Card.Title>
                         <Card.Text>
                             <form>
                                 <div>
                                     <h5>ログインID</h5>
-                                    <input />
+                                    <input
+                                        name="loginId"
+                                        placeholder="ログインIDを入力してください"
+                                        onChange={(e) =>
+                                            setLoginId(e.target.value)
+                                        }
+                                    />
                                 </div>
                                 <div style={{ marginTop: "10px" }}>
                                     <h5>パスワード</h5>
-                                    <input />
+                                    <input
+                                        name="password"
+                                        placeholder="パスワードを入力してください"
+                                        onChange={(e) =>
+                                            setPassword(e.target.value)
+                                        }
+                                    />
                                 </div>
                                 <button className="">LOGIN</button>
                             </form>
